819fe7e62a732c71c86ca9965af42117d3cb699f
pulkitpn <vlpulkit.773@gmail.com>
2021-11-02 14:35:05 +0530

    react_programs

---
+++ myapp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Restaurent from './component/basics/Restaurent'
+
+const App = () => {
+  return (
+    <div>
+     
+     <Restaurent />
+     
+    </div>
+  )
+}
+
+export default App
